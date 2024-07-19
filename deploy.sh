2f0511b3fcf07e028dc6c8cc9b3fd49951c8e302
Cameron Karagitz <ckaragitz@gmail.com>
2024-07-19 15:29:32 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,22 @@
 # Set the project ID and region
 PROJECT_ID="amfam-claims"
 REGION="us-central1"
+SERVICE_ACCOUNT="function@amfam-claims.iam.gserviceaccount.com"
+APP_NAME="claims"
 
 # Build the Docker image
 gcloud builds submit --tag us-docker.pkg.dev/$PROJECT_ID/container/amfam-claims --source .
 
-# Deploy the image to Cloud Run
-gcloud run deploy amfam-claims \
-  --image us-docker.pkg.dev/$PROJECT_ID/container/amfam-claims \
-  --region $REGION \
+gcloud run deploy $APP_NAME \
+  --project $PROJECT_ID \
+  --image us-docker.pkg.dev/$PROJECT_ID/container/$APP_NAME:latest \
   --platform managed \
-  --allow-unauthenticated
+  --region $REGION \
+  --port 8080 \
+  --cpu 1 \
+  --memory 1000Mi \
+  --concurrency 80 \
+  --timeout 3000 \
+  --service-account $SERVICE_ACCOUNT \
+  --allow-unauthenticated \
+  --clear-env-vars
